c411d31e9e64375d9f1183fe05342d1d0d5bb9c0
João Carvalho Oliveira <joao.c.oliveira@inesctec.pt>
2024-08-28 11:22:53 +0100

    First Commit

---
+++ ReaderPublisher/ReaderPublisher.cpp
@@ -0,0 +1,269 @@
+// Using SDL and standard IO
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <chrono>
+#include <mosquitto.h>
+#include <fstream>
+
+#define JS_RECONNECT
+
+#define DEFAULT_ADDRESS "127.0.0.1"
+#define DEFAULT_PORT 1883
+#define TOPIC "/moses/cmd_vel"
+#define QOS 0
+#define TIMEOUT 10000
+#define JOY_READ_INTERVAL 100
+#define JOY_RETRY_INTERVAL 1000
+#define V_MAX 1
+#define VN_MAX 1
+#define W_MAX 1
+
+struct mosquitto* mosq = NULL;
+
+// Analog joystick dead zone
+const int JOYSTICK_DEAD_ZONE = 8000;
+SDL_Joystick* js = NULL;
+SDL_TimerID readID = NULL, retryID = NULL;
+SDL_Event event;
+bool change_timer = false, prev_connection_state = true, connection_state = true, retry_connection = false;
+int num_retries = 0;
+
+int js_num_axes = 0;
+int js_num_buttons = 0;
+int js_num_hats = 0;
+
+int v_axis, vn_axis, w_axis;
+
+bool sdl_init()
+{
+    // Initialization flag
+    bool success = true;
+
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) < 0)
+    {
+        printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+        success = false;
+    }
+
+    SDL_JoystickEventState(SDL_ENABLE);
+
+    if (SDL_NumJoysticks() < 1)
+    {
+        printf("Warning: No joysticks connected!\n");
+        success = false;
+    }
+    else
+    {
+        // Load joystick
+        js = SDL_JoystickOpen(0);
+        if (js == NULL)
+        {
+            printf("Warning: Unable to open game controller! SDL Error: %s\n", SDL_GetError());
+            success = false;
+        }
+        else
+        {
+            printf("Joystick connected\n");
+            printf("Name: %s\n", SDL_JoystickNameForIndex(0));
+            js_num_axes = SDL_JoystickNumAxes(js);
+            printf("Num axes: %d\n", js_num_axes);
+            js_num_buttons = SDL_JoystickNumButtons(js);
+            printf("Num buttons: %d\n", js_num_buttons);
+            js_num_hats = SDL_JoystickNumHats(js);
+            printf("Num hats: %d\n", js_num_hats);
+        }
+    }
+
+    return success;
+}
+
+void sdl_close()
+{
+    // Close game controller
+    SDL_JoystickClose(js);
+    js = NULL;
+    SDL_Quit();
+}
+
+bool read_ip_config(std::string& address, int& port)
+{
+    std::ifstream config_file("ipconf");
+    if (!config_file.is_open())
+    {
+        fprintf(stderr, "Error: Unable to open ipconf file.\n");
+        return false;
+    }
+
+    std::string line;
+    if (std::getline(config_file, line))
+    {
+        std::istringstream iss(line);
+        if (!(iss >> address >> port))
+        {
+            fprintf(stderr, "Error: Invalid format in ipconf file.\n");
+            return false;
+        }
+    }
+    else
+    {
+        fprintf(stderr, "Error: ipconf file is empty.\n");
+        return false;
+    }
+
+    config_file.close();
+    return true;
+}
+
+bool mosquitto_init(const std::string& address, int port)
+{
+    int rc;
+    mosquitto_lib_init();
+
+    mosq = mosquitto_new(NULL, true, NULL);
+    if (!mosq)
+    {
+        fprintf(stderr, "Error: Out of memory when creating Mosquitto instance.\n");
+        return false;
+    }
+
+    rc = mosquitto_connect(mosq, address.c_str(), port, TIMEOUT);
+    if (rc != MOSQ_ERR_SUCCESS)
+    {
+        fprintf(stderr, "Unable to connect to the broker. Return code: %d\n", rc);
+        mosquitto_destroy(mosq);
+        mosquitto_lib_cleanup();
+        return false;
+    }
+    return true;
+}
+
+void mosquitto_close()
+{
+    mosquitto_disconnect(mosq);
+    mosquitto_destroy(mosq);
+    mosquitto_lib_cleanup();
+}
+
+Uint32 RetryCallback(Uint32 interval, void* param)
+{
+    retry_connection = true;
+    return interval;
+}
+
+Uint32 ReadCallback(Uint32 interval, void* param)
+{
+    std::stringstream json;
+
+    bool d_switch = SDL_JoystickGetButton(js, 4);
+    const char* led = SDL_JoystickGetButton(js, 0) ? "true" : "false";
+
+    json << "{" << std::endl;
+    json << "\"v\":" << d_switch * -1 * SDL_JoystickGetAxis(js, 1) * V_MAX / 32765 << "," << std::endl;
+    json << "\"vn\":" << d_switch * SDL_JoystickGetAxis(js, 0) * VN_MAX / 32765 << "," << std::endl;
+    json << "\"w\":" << d_switch * -1 * SDL_JoystickGetAxis(js, 2) * W_MAX / 32765 << std::endl;
+    json << "}" << std::endl;
+
+    std::string aux;
+    const char* message;
+    aux = json.str();
+    message = aux.c_str();
+    std::cout << message << std::endl;
+    int rc = mosquitto_publish(mosq, NULL, TOPIC, strlen(message), message, QOS, false);
+    if (rc != MOSQ_ERR_SUCCESS)
+    {
+        fprintf(stderr, "Failed to publish message. Return code: %d\n", rc);
+    }
+    else
+    {
+        printf("Message published successfully.\n");
+    }
+
+    return interval;
+}
+
+int main(int argc, char* argv[])
+{
+    // Game Controller 1 handler
+    int rc;
+    std::cout << "Initializing SDL\n";
+    if (!sdl_init())
+    {
+        return 0;
+    }
+
+    if (SDL_Init(SDL_INIT_TIMER) < 0)
+    {
+        printf("SDL timer could not initialize! SDL Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    std::cout << "SDL successfully initialized\n";
+
+    std::string address = DEFAULT_ADDRESS;
+    int port = DEFAULT_PORT;
+
+    if (!read_ip_config(address, port))
+    {
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "Initializing mosquitto\n";
+    if (!mosquitto_init(address, port))
+    {
+        return EXIT_FAILURE;
+    }
+    std::cout << "Mosquitto successfully initialized\n";
+
+    std::string aux;
+    const char* message;
+
+    readID = SDL_AddTimer(JOY_READ_INTERVAL, ReadCallback, js);
+    while (1)
+    {
+        connection_state = SDL_JoystickGetAttached(js);
+        change_timer = (prev_connection_state != connection_state);
+        prev_connection_state = connection_state;
+
+        if (!connection_state) // Disconnected
+        {
+            if (change_timer)
+            {
+                std::cout << "Change Timer!\n";
+                SDL_RemoveTimer(readID);
+                sdl_close();
+                retryID = SDL_AddTimer(JOY_RETRY_INTERVAL, RetryCallback, js);
+            }
+            if (retry_connection)
+            {
+                std::cout << "Retry connection!\n";
+                while (SDL_PollEvent(&event))
+                {
+                }
+                sdl_init();
+                retry_connection = false;
+            }
+        }
+        else // Connected
+        {
+            if (change_timer)
+            {
+                std::cout << "Joystick reconnected!\n";
+                SDL_RemoveTimer(retryID);
+                readID = SDL_AddTimer(JOY_READ_INTERVAL, ReadCallback, js);
+            }
+            while (SDL_PollEvent(&event))
+            {
+            }
+        }
+        SDL_Delay(10);
+    }
+
+    mosquitto_close();
+    return EXIT_SUCCESS;
+}
---
+++ Subscriber/Subscriber.cpp
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <chrono>
+#include <mosquitto.h>
+#include <json.hpp>
+
+#define ADDRESS "127.0.0.1"//"10.122.241.72" // Replace with your MQTT broker address
+#define PORT 1883     // Replace with your MQTT broker port
+#define TOPIC "/moses/cmd_vel"         // Replace with the topic you want to subscribe to
+
+#define BUFFER_SIZE 10;
+long long buffer[10] = { 0 };
+long long avg_latency = 0;
+
+void moving_avg(long long new_value)
+{
+    long long lt_sum = 0;
+    for (int i = 10 - 1; i > 0; i--)
+    {
+        buffer[i] = buffer[i - 1];
+        lt_sum += buffer[i];
+    }
+    buffer[0] = new_value;
+
+    lt_sum += buffer[0];
+    avg_latency = lt_sum / 10;
+}
+
+void on_message_callback(struct mosquitto* mosq, void* userdata, const struct mosquitto_message* message)
+{
+    /* printf("Received message on topic %s: %s\n", message->topic, (char *)message->payload);
+    auto now = std::chrono::system_clock::now();
+    auto timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count(); */
+    try
+    {
+        nlohmann::json json = nlohmann::json::parse((char*)message->payload);
+        /* long long timestamp_message = json["timestamp"];
+        printf("Latency: %lld\n", timestamp - timestamp_message);
+        moving_avg(timestamp - timestamp_message);
+        std::cout << "Average latency (last 10 messages): " << avg_latency << std::endl; */
+        std::cout << "v: " << json["v"] << std::endl;
+        std::cout << "vn: " << json["vn"] << std::endl;
+        std::cout << "w: " << json["w"] << std::endl;
+        std::cout << std::endl;
+        return;
+    }
+    catch (const std::exception& e)
+    {
+        printf("Couldn't parse json\n");
+        return;
+    }
+}
+
+int main()
+{
+    struct mosquitto* mosq = NULL;
+
+    mosquitto_lib_init(); // Initialize the Mosquitto library
+
+    mosq = mosquitto_new("mqtt_c_subscriber", true, NULL);
+    if (!mosq)
+    {
+        fprintf(stderr, "Error: Unable to create Mosquitto instance.\n");
+        return 1;
+    }
+
+    mosquitto_message_callback_set(mosq, on_message_callback);
+
+    printf("connecting...\n");
+
+    if (mosquitto_connect(mosq, ADDRESS, PORT, 60) != MOSQ_ERR_SUCCESS)
+    {
+        fprintf(stderr, "Error: Unable to connect to the broker.\n");
+        mosquitto_destroy(mosq);
+        mosquitto_lib_cleanup();
+        return 1;
+    }
+
+    if (mosquitto_subscribe(mosq, NULL, TOPIC, 0) != MOSQ_ERR_SUCCESS)
+    {
+        fprintf(stderr, "Error: Unable to subscribe to the topic.\n");
+        mosquitto_destroy(mosq);
+        mosquitto_lib_cleanup();
+        return 1;
+    }
+
+    fprintf(stderr, "CONNECTION SUCCESSFULL!.\n");
+    while (1)
+    {
+        if (mosquitto_loop(mosq, -1, 1) != MOSQ_ERR_SUCCESS)
+        {
+            fprintf(stderr, "Error: Mosquitto loop failed.\n");
+            break;
+        }
+    }
+
+    mosquitto_disconnect(mosq);
+    mosquitto_destroy(mosq);
+    mosquitto_lib_cleanup();
+
+    return 0;
+}
